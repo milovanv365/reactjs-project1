834a853c19c14ad5b72fd85590475769fd43da56
fullstack <fullstack@test.com>
2020-03-15 10:07:17 +0100

    Updates

---
+++ precache-manifest.89000c264351ddc7c07ca6f7819235ba.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dcaf8780149a556167d9221d4c86216",
+    "url": "/reactjs-odd/index.html"
+  },
+  {
+    "revision": "ac1ee414b760f68007a7",
+    "url": "/reactjs-odd/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "16650b3e7a7324286684",
+    "url": "/reactjs-odd/static/css/main.86b5ce98.chunk.css"
+  },
+  {
+    "revision": "ac1ee414b760f68007a7",
+    "url": "/reactjs-odd/static/js/2.c3d0d7ce.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/reactjs-odd/static/js/2.c3d0d7ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16650b3e7a7324286684",
+    "url": "/reactjs-odd/static/js/main.777e115b.chunk.js"
+  },
+  {
+    "revision": "11eac601b185ad5a3d86",
+    "url": "/reactjs-odd/static/js/runtime-main.be6ae651.js"
+  },
+  {
+    "revision": "0dc9f79113908c225755f28131651bf8",
+    "url": "/reactjs-odd/static/media/bg-home1-lg.0dc9f791.jpg"
+  },
+  {
+    "revision": "0cf400efd3f4fb500909eac9a509c312",
+    "url": "/reactjs-odd/static/media/bg-home1-sm.0cf400ef.jpg"
+  },
+  {
+    "revision": "ff3f86d2a6faa648f5eaa3a463867726",
+    "url": "/reactjs-odd/static/media/bg-home2.ff3f86d2.jpg"
+  },
+  {
+    "revision": "48a4930ae3b98bf826084f4cde00bbd5",
+    "url": "/reactjs-odd/static/media/bg-home3.48a4930a.jpg"
+  },
+  {
+    "revision": "79c08ddf92d2d4717ee25caab743c0f1",
+    "url": "/reactjs-odd/static/media/product1.79c08ddf.png"
+  },
+  {
+    "revision": "2c1af8fd02f6c93d126b28df9141689a",
+    "url": "/reactjs-odd/static/media/touchstable-bg1.2c1af8fd.png"
+  },
+  {
+    "revision": "e5362141be6377c5cce72ce414edf236",
+    "url": "/reactjs-odd/static/media/touchstable-brand1.e5362141.png"
+  },
+  {
+    "revision": "c3b2e3a8f75d301469709d7d017eeaf9",
+    "url": "/reactjs-odd/static/media/touchstable-brand2.c3b2e3a8.png"
+  },
+  {
+    "revision": "40b06738b0781f5dcbc3ba9fbc45b65c",
+    "url": "/reactjs-odd/static/media/touchstable-brand3.40b06738.png"
+  },
+  {
+    "revision": "5f2bb62b0c4ec2417f1ad2518e60db38",
+    "url": "/reactjs-odd/static/media/touchstable-person1.5f2bb62b.jpg"
+  },
+  {
+    "revision": "7d2a970e067e73e33661b467e87a135b",
+    "url": "/reactjs-odd/static/media/touchstable-person2.7d2a970e.jpg"
+  },
+  {
+    "revision": "50bde7f7bb400b212b8432c64686dd3b",
+    "url": "/reactjs-odd/static/media/touchstable-person3.50bde7f7.jpg"
+  },
+  {
+    "revision": "de6e98c7020f84a3decc2b7e98d96969",
+    "url": "/reactjs-odd/static/media/touchstable-person4.de6e98c7.jpg"
+  },
+  {
+    "revision": "21481f13baeedee595dc993ae2614ee1",
+    "url": "/reactjs-odd/static/media/touchstable-person5.21481f13.jpg"
+  },
+  {
+    "revision": "fa8ea48eeb531c5bfbc3757d64e2be64",
+    "url": "/reactjs-odd/static/media/touchstable-person6.fa8ea48e.jpg"
+  },
+  {
+    "revision": "e1eb1c9221e0a22b51ad0720e724d880",
+    "url": "/reactjs-odd/static/media/touchstable-person7.e1eb1c92.jpg"
+  }
+]);
